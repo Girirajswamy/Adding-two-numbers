171630f3e82ecf7ff27caaacb4f55acaf48dd657
Girirajswamy <grswamy2016@gmail.com>
2025-02-24 14:38:20 +0530

    hello

--- src/main.c
+++ src/main.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 #include"addition.h"
 int main(){
-    int a,b;
+    int a,b,result;
     printf("enter the value1\n");
     scanf("%d",&a);
     printf("enter the value2\n");
     scanf("%d",&b);
-    printf("addition of %d and %d = %d",a,b,addition(a,b));
+    result = addition(a,b);
+    printf("addition of %d and %d = %d",a,b,result);
 }
